17eb78581df4f153ef546d8d7df0c22f7a654920
masahiro1005 <masa.sub.hiro@gmail.com>
2026-05-30 17:51:08 +0900

    ユーザー登録、ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :first_name,
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name_kana,
+              format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :first_name_kana,
+              format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :birthday
+  end
+
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                      message: 'には英字と数字の両方を含めて設定してください' }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

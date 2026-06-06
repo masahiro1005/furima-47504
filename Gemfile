8a5fa2d45f96889d095bdbb3ec1826458d1681bd
masahiro1005 <masa.sub.hiro@gmail.com>
2026-06-06 12:38:22 +0900

    商品一覧表示と商品作成機能を実装

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ group :production do
 end
 
 gem "devise"
+
+gem "active_hash"
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,35 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = current_user.items.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_fee_id,
+      :prefecture_id,
+      :shipping_day_id,
+      :price
+    )
   end
 end
